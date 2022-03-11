f5dbaac0857db30debc2fb452c5c4604c0fb114c
precious123-gifted <58713062+precious123-gifted@users.noreply.github.com>
2022-03-11 23:06:37 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,8 +17,7 @@ int main(void)
 		printf("%i is positive\n", n);
 	else if (n < 0)
 		printf("%i is negative\n", n);
-	else
-		printf("%i is zero\n", n);
+	else printf("%i is zero\n", n);
 
 	return (0);
 }
