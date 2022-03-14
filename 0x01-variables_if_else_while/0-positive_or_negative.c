cb605a33384c2e2c66d45b243f8bdc431f329d14
precious123-gifted <ogunameprecious123@hmail.com>
2022-03-15 00:36:40 +0100

    true

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,7 +13,8 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 printf("%i is positive\n", n);
-else	if	(n < 0)
+else if(n < 0)
 printf("%i is negative\n", n);
-else	printf("%i is zero\n", n);
+else
+printf("%i is zero\n", n);
 return	(0);}
