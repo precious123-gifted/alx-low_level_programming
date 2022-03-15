3ec462f97445b5d43410a275d8348813fcfaea10
precious123-gifted <58713062+precious123-gifted@users.noreply.github.com>
2022-03-15 23:26:36 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,18 @@
 #include "main.h"
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-                char chaine[] = "_putchar\n";
-                int i = 0;
-
-                while (chaine[i])
-                {
-                        _putchar(chaine[i]);
-                        i++;
-                }
-                return (0);
+  char chaine[] = "_putchar\n";
+  int i = 0;
+  while (chaine[i])
+  {
+    _putchar(chaine[i]);
+    i++;
+  }
+  return (0);
 }
