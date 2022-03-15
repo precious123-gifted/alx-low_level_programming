acb92818bfbcd7c080e04c599ec1ce2aeabb0394
precious123-gifted <58713062+precious123-gifted@users.noreply.github.com>
2022-03-15 02:27:17 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,4 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include <stdlib.h>	#include <time.h>	#include <stdio.h>
 /**
  * main - main block
  * Description: Print statements based on the last digit of the random number.
