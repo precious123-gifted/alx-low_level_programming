e9c7f9a581feb94da3ce0a62d0c70b850ac0e817
precious123-gifted <58713062+precious123-gifted@users.noreply.github.com>
2022-03-15 02:09:40 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,10 +13,10 @@ int main(void)
 	srand(time(0));
 	n=rand()-RAND_MAX/2;
 	if(n>0)
-		printf("%1 is positive\n",n);
+		printf("%i is positive\n",n);
 	else if(n<0)
-		printf("%1 is negative\n",n);
+		printf("%i is negative\n",n);
 	else
-		printf("%1 is zero\n",n);
+		printf("%i is zero\n",n);
 	return(0);
 }
