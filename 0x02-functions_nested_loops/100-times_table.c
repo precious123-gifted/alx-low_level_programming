c0105fdc5381f30464cbcca129992f322ba7d43d
precious123-gifted <58713062+precious123-gifted@users.noreply.github.com>
2022-03-17 05:35:49 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,13 +1,12 @@
 #include "main.h"
-
 /**
- *  print_times_table - Prints the n times table
- *
- *  @n: number times table (0 < n <= 15)
- *
- *  Return: no return
- */
-void print_times_table(int n)
+*  print_times_table - Prints the n times table
+*
+*  @n: number times table (0 < n <= 15)
+*
+*  Return: no return
+*/
+void print_times_table(int)
 {
 	int a, b, op;
 
