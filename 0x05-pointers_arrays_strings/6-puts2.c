2269fec9d5abb15ca450623e4b0ddabe83950f64
precious123-gifted <58713062+precious123-gifted@users.noreply.github.com>
2022-03-22 01:21:47 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,24 +1,22 @@
+
 #include "main.h"
 
 /**
- * puts2 - prints one char out of 2
- * @str: type
+ * puts2 - a function that takes a pointer to an int as parameter and
+ * @str: chaine of caractere
  *
+ * Return: 1 or 0
  */
 
 void puts2(char *str)
 {
-	int i, j = 0;
+	int i = 0;
 
-	while (str[i] != '\0')
+	while (str[i])
 	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
 		i++;
-	}
-
-	while (j < i)
-	{
-		_putchar(str[j]);
-		j = j + 2;
 	}
 	_putchar('\n');
 }
