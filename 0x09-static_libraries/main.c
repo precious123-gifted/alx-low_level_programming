f201794078c35ff98fcd423234b0779172694055
precious123-gifted <58713062+precious123-gifted@users.noreply.github.com>
2022-04-02 05:22:27 +0100

    Add files via upload

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,15 @@
 #include "main.h"
 
+#include <unistd.h>
+
+/**
+ * main - Main entry point for the function.
+ *
+ * Return: 0 on success.
+ */
+
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	_puts("\"At the end of the day est hacker\"\n\t- Kevin Mitnick");
+	return (0);
 }
