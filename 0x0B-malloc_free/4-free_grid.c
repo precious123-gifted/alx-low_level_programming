1b1665afefc8abe76fb15f68fe12ae735d4eb806
precious123-gifted <58713062+precious123-gifted@users.noreply.github.com>
2022-04-06 04:56:41 +0100

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,19 +1,22 @@
-#include<stdlib.h>
-#include<stdio.h>
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * free_grid - a function ...
- * @grid: the matrice
- * @height: the number
+ * free_grid - frees a 2 dimensional grid previously created by
+ * alloc_grid function.
+ * @grid: matrix double pointer
+ * @height: rows (pointer)
  *
- * Return: 1 or 0
+ * Return: Nothing
  */
-
 void free_grid(int **grid, int height)
 {
-	int i = 0;
+	int i;
 
 	for (i = 0; i < height; i++)
-		free(grid[i]);
+	{
+		free(*(grid + i));
+	}
 	free(grid);
 }
