153751eeb1f46a333506e648a9b869b7d2037d26
precious123-gifted <58713062+precious123-gifted@users.noreply.github.com>
2022-04-09 02:09:28 +0100

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -13,6 +13,10 @@ int *array_range(int min, int max);
 void errors(void);
 int _strdigit(char *s);
 void _puts(char *s);
+void rev_num_str(int start, int end, char *ns);
+int _strlen(char *s);
+char *strmul(char *a, char *b);
+int main(int argc, char *argv[]);
 int is_digit(char *s);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 #endif
