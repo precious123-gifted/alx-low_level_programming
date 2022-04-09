1590602600dbfaaf64bb2d61005eb873443198e1
precious123-gifted <58713062+precious123-gifted@users.noreply.github.com>
2022-04-09 02:03:22 +0100

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,7 +11,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void errors(void);
-int _strlen(char *s);
+int _strdigit(char *s);
 int is_digit(char *s);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 #endif
